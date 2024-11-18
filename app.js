df90683f93693dd80757800df14c1dbd41b2dc73
Adrián Clavario <adrianclavario@gmail.com>
2024-11-18 07:52:42 -0300

    Agrego archivos del trabajo practico 15 al repositorio

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const peliculas = require('./peliculas.js');
+
+function mostrarPeliculas(array){
+let element = "";
+  for (let i = 0; i < array.length; i++) {
+ console.log(element = array[i]);
+  }
+  return element;
+}
+
+console.log(mostrarPeliculas(peliculas));
+
+
+const texto = fs.readFileSync('./mensaje.txt', 'utf8');
+    console.log('Mensaje:', texto);
---
+++ peliculas.js
@@ -0,0 +1,77 @@
+const peliculas = [
+  {
+    id: 1,
+    title: "Back to the future",
+    rating: 8.8,
+    awards: 5,
+    length: 148,
+    price: 15,
+    genre: "Aventuras"
+  },
+  {
+    id: 2,
+    title: "Lord of the rings: The return of the king",
+    rating: 8.9,
+    awards: 11,
+    length: 201,
+    price: 18,
+    genre: "Aventuras"
+  },
+  {
+    id: 3,
+    title: "Superman",
+    rating: 7.4,
+    awards: 1,
+    length: 143,
+    price: 12,
+    genre: "Acción"
+  },
+  {
+    id: 4,
+    title: "The Hobbit: An Unexpected Journey",
+    rating: 7.8,
+    awards: 3,
+    length: 169,
+    price: 16,
+    genre: "Aventuras"
+  },
+  {
+    id: 5,
+    title: "KISS Meets the Phantom of the Park",
+    rating: 4.3,
+    awards: 0,
+    length: 96,
+    price: 8,
+    genre: "Suspenso"
+  },
+  {
+    id:6,
+    title: "Anchorman: The Legend of Ron Burgundy",
+    rating: 7.1, 
+    awards: 0, 
+    length: 94, 
+    price: 10,
+    genre: "Comedia"
+  },
+  {
+    id: 7,
+    title: "The Conjuring",
+    rating: 7.5, 
+    awards: 15, 
+    length: 112,
+    price: 12,
+    genre: "Terror"
+},
+{
+  id: 8,
+  title: "The Lion King",
+  rating: 8.5, 
+  awards: 2, 
+  length: 88,
+  price: 14,
+  genre: "Animación"
+}
+];
+
+
+module.exports = peliculas;
